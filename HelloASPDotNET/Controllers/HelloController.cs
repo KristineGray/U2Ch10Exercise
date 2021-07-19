d013496a555bb8954b95ecb2025d1e09c0636cd5
KristineGray <krisgraya@gmail.com>
2021-07-19 16:35:04 -0400

    created language selector and five language options in HelloWorld Index

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -9,24 +9,42 @@ namespace HelloASPDotNET.Controllers
     [Route("/helloworld/")]
     public class HelloController : Controller
     {
-        // GET: /<controller>/
+        /* English = Hello
+         * French = Bonjour
+         * Spanish = Hola
+         * Pashto = Salaam
+         * Binary = 0110100001100101011011000110110001101111
+        */
+
         [HttpGet]
         public IActionResult Index()
         {
-            string html = "<form method='post' action='/helloworld/'>" + "<input type='text' name='name' />" + "<input type='submit' value='Greet Me!' />" + "</form>";
+            string formOpen = "<form method='post' action='/helloworld/'>";
+            string formClose = "</form>";
+            string inputNameText = "<input type='text' name='name' />";
+            string inputLangSelector = "<select name='language'>";
+            string langOptionEng = "<option value='English'>English</option>";
+            string langOptionPashto = "<option value='Pashto'>Pashto</option>";
+            string langOptionFrench = "<option value='French'>French</option>";
+            string langOptionSpanish = "<option value='Spanish'>Spanish</option>";
+            string langOptionBinary = "<option value='Binary'>Binary</option>";
+            string langChoice = langOptionEng + langOptionFrench + langOptionPashto + langOptionSpanish + langOptionBinary;
+
+            string inputSubmit = "<input type='submit' value='Greet Me!' />";
+
+            string html =  formOpen + inputNameText + inputLangSelector + langChoice + inputSubmit + formClose;
 
             return Content(html, "text/html");
         }
 
-        // GET: /hello/welcome
-        //[HttpGet]
-        //[Route("helloworld/welcome/{name?}")]
-        
         [HttpGet("welcome/{name?}")]
         [HttpPost]
         public IActionResult Welcome(string name = "World")
         {
             return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
         }
+
+        
+
     }
 }
